dbb200b63acca885c34bd38520418644e25200b1
William Palermo <palermow@gmail.com>
2018-05-01 12:58:16 -0300

    testing

---
+++ reactivex/src/test/java/com/wpalermo/reactivex/AppTest.java
@@ -0,0 +1,107 @@
+package com.wpalermo.reactivex;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.reactivex.Observable;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	private String result;
+
+	@Test
+	public void observableJust() {
+
+		Observable<String> observable = Observable.just("Teste1");
+		observable.subscribe(s -> result = s);
+
+		assertTrue(result.equals("Teste1"));
+
+	}
+
+	@Test
+	public void observableFrom() {
+
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g" };
+		result = new String();
+
+		Observable<String> observable = Observable.fromArray(letters);
+		observable.subscribe(i -> result += i, 
+						     Throwable::printStackTrace, 
+						     () -> result += "_completed");
+
+		assertTrue(result.equals("abcdefg_completed"));
+
+	}
+	
+	/**
+	 * Map -> aplica uma funcao alterando cada um dos opertators emitidos por um observable. (descartando o anterior) 
+	 *  
+	 */
+	@Test
+	public void observableFromTransformation() {
+
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g" };
+		result = new String();
+
+		Observable.fromArray(letters)
+			.map(String::toUpperCase)
+			.subscribe(letter -> result += letter);
+
+		assertTrue(result.equals("ABCDEFG"));
+
+	}
+	
+	/**
+	 * SCAN -> aplica uma funcao alterando cada um dos opertators emitidos por um observable. 
+	 * 			Porem, diferente do MAP, mantem o estado anterior permitindo que se carregue o estado atual para os proximos eventos/
+	 */
+	@Test
+	public void observableFromScan() {
+
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g" };
+		result = new String();
+		
+		Observable.fromArray(letters)
+			.scan(new StringBuilder(), StringBuilder::append)
+			.subscribe(letter -> result += letter);
+
+		
+		assertTrue(result.equals("aababcabcdabcdeabcdefabcdefg"));
+
+	}
+	
+	
+	private String pares;
+	private String impares;
+	
+	@Test
+	public void observableFromGroupBy() {
+
+		Integer[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+		result = new String();
+		pares = new String();
+		impares = new String();
+		
+		Observable.fromArray(numbers)
+			.groupBy(key -> 0 == (key % 2) ? "PAR" : "IMPAR" )
+			.subscribe(group -> 
+					group.subscribe(number -> {
+						if(group.getKey().toString().equals("PAR"))
+							pares += number;
+						else
+							impares += number;
+					}));
+				
+		assertTrue(pares.equals("246"));
+		assertTrue(impares.equals("1357"));
+
+		
+
+	}
+
+}
